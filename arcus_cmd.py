73f2e6971da986dd36bc743174a2d27f56239016
devleoper-account <irteam@xdash01.arcus>
2014-09-26 20:14:22 +0900

    refactoring

--- arcus_cmd.py
+++ arcus_cmd.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 #
 # arcus-python-client - Arcus python client drvier
 # Copyright 2014 NAVER Corp.
@@ -19,6 +21,7 @@
 import sys
 import telnetlib
 import re
+import socket
 
 from kazoo.client import KazooClient
 import kazoo
@@ -28,36 +31,74 @@ import paramiko
 
 lists = []
 
-def get_arcus_node_list(addr, code):
-	zk = KazooClient(addr)
-	zk.start()
-	children = zk.get_children('/arcus/cache_list/' + code)
 
-	if code == '':
+class arcus_node:
+	def __init__(self, ip, port, name, code):
+		self.ip = ip
+		self.port = port 
+		self.name = name
+		self.code = code
+
+	def __repr__(self):
+		if self.name and self.code:
+			return '[%s:%s-(%s,%s)]' % (self.ip, self.port, self.name, self.code)
+		elif self.name:
+			return '[%s:%s-(%s)]' % (self.ip, self.port, self.name)
+		elif self.code:
+			return '[%s:%s-(%s)]' % (self.ip, self.port, self.code)
+
+		return '[%s:%s]' % (self.ip, self.port)
+
+	def do_arcus_command(self, command):
+		tn = telnetlib.Telnet(self.ip, self.port)
+		tn.write(bytes(command + '\n', 'utf-8'))
+		result = tn.read_until(bytes('END', 'utf-8'))
+		result = result.decode('utf-8');
+		tn.write(bytes('quit\n', 'utf-8'))
+		tn.close()
+		return result;
+
+
+class zookeeper:
+	def __init__(self, address):
+		self.address = address
+		self.zk = KazooClient(address)
+		self.zk.start()
+
+	def get_arcus_cache_list(self):
+		children = self.zk.get_children('/arcus/cache_list/')
 		return children
 
-	ret = []
-	for child in children:
-		addr, name = child.split('-')
-		ip, port = addr.split(':')
-		ret.append((name, port, ip))
+	def get_arcus_node_of_code(self, code, server):
+		children = self.zk.get_children('/arcus/cache_list/' + code)
 
-	return ret
+		ret = []
+		for child in children:
+			addr, name = child.split('-')
+			ip, port = addr.split(':')
+
+			if server != '' and (server != ip and server != name):
+				continue # skip this
+
+			node = arcus_node(ip, port, name, None)
+			ret.append(node)
+
+		return ret
 
 
-def get_arcus_service_list(addr, ip):
-	zk = KazooClient(addr)
-	zk.start()
-	children = zk.get_children('/arcus/cache_server_mapping/')
+	def get_arcus_node_of_server(self, addr):
+		ip = socket.gethostbyname(addr)
+		children = self.zk.get_children('/arcus/cache_server_mapping/')
 
-	ret = []
-	for child in children:
-		l = len(ip)
-		if child[:l] == ip:
-			service = zk.get_children('/arcus/cache_server_mapping/' + child)
-			ip, port = child.split(':')
-			ret.append((ip, port, service[0]))
-	return ret
+		ret = []
+		for child in children:
+			l = len(ip)
+			if child[:l] == ip:
+				code = self.zk.get_children('/arcus/cache_server_mapping/' + child)
+				ip, port = child.split(':')
+				ret.append(arcus_node(ip, port, None, code[0]))
+		return ret
+
 
 def do_ssh_command(addr, command):
 	ssh = paramiko.SSHClient()
@@ -69,14 +110,6 @@ def do_ssh_command(addr, command):
 		sys.stdout.write(line)
 	ssh.close()
 
-def do_arcus_command(ip, port, command):
-	tn = telnetlib.Telnet(ip, port)
-	tn.write(bytes(command + '\n', 'utf-8'))
-	result = tn.read_until(bytes('END', 'utf-8'))
-	result = result.decode('utf-8');
-	tn.write(bytes('quit\n', 'utf-8'))
-	return result;
-
 
 if __name__ == '__main__':
 	usage = "usage: %prog [options]"
@@ -84,16 +117,12 @@ if __name__ == '__main__':
 	parser.add_option('-f', '--file', dest='file', default='', help='zookeeper address lists file')
 	parser.add_option('-a', '--address', dest='address', default='', help='zookeeper address')
 	parser.add_option('-s', '--service', dest='service', default='', help='service code')
-	parser.add_option('-c', '--command', dest='command', default='', help='command')
-	parser.add_option('-n', '--node', dest='node', default='', help='node ip')
+	parser.add_option('-c', '--command', dest='command', default='', help='arcus command')
+	parser.add_option('-n', '--node', dest='node', default='', help='node address or ip')
 	parser.add_option('-x', '--ssh_command', dest='ssh_command', default='', help='ssh command execution')
-	parser.add_option('-m', '--memory', dest='memory', default=False, help='memory info', action='store_true')
+	parser.add_option('-i', '--i', dest='info', default=False, help='memory, maxconns info', action='store_true')
 
 	(options, args) = parser.parse_args()
-
-	if options.service and options.node:
-		print('-s(--service) and -n(--node) is exclusive')
-		exit()
 
 	if options.file:
 		fh = open(options.file)
@@ -104,77 +133,89 @@ if __name__ == '__main__':
 
 	for address in addresses:
 		try:
+			print ('\n\n## Zookeeper address %s' % address)
+			zoo = zookeeper(address)
+
 			if options.service:
-				lists = get_arcus_node_list(address, options.service)
+				lists = zoo.get_arcus_node_of_code(options.service, options.node)
 			elif options.node:
-				lists = get_arcus_service_list(address, options.node)
+				lists = zoo.get_arcus_node_of_server(options.node)
 			else:
-				lists = get_arcus_node_list(address, '') # print all cache_list
+				lists = zoo.get_arcus_cache_list()
+				print (lists)
+				continue
 
 		except Exception as e:
+			print(e)
 			# not found
 			continue
 
-		print ('## Zookeeper address %s' % address)
 		break
 
 
-	lists.sort()
-
-	print (lists)
+	lists.sort(key = lambda x: x.ip+x.port)
+	for node in lists:
+		print(node)
 
 	if options.ssh_command:
-		do_ssh_command(lists[0][0], options.ssh_command)
+		do_ssh_command(lists[0].ip, options.ssh_command)
 
 
 	if options.command:
 		for node in lists:
 			try:
-				result = do_arcus_command(node[0], node[1], options.command)
-				print ('[%s:%s(%s)]\t\t%s - %s' % (node[0], node[1], node[2], options.command, result))
+				result = node.do_arcus_command(options.command)
+				print ('%s\t\t%s - %s' % (node, options.command, result))
 			except Exception as e:
-				print ('[%s:%s(%s)]\t\tFAILED!!' % (node[0], node[1], node[2]))
+				print ('%s\t\tFAILED!!' % (node))
 				print(e)
 				
 
-	if options.memory:
+	if options.info:
 		if options.node:
-			print('===============================================================')
-			print ('[%s] system memory' % lists[0][0]);
-			do_ssh_command(lists[0][0], 'free') # run once
-			print('---------------------------------------------------------------')
+			print('===================================================================================')
+			print ('[%s] system memory' % lists[0].ip);
+			do_ssh_command(lists[0].ip, 'free') # run once
+			print('-----------------------------------------------------------------------------------')
 
 
 		re_limit = re.compile("STAT limit_maxbytes ([0-9]+)")
 		re_bytes = re.compile("STAT bytes ([0-9]+)")
+		re_curr_conn = re.compile("STAT curr_connections ([0-9]+)")
+		re_maxconns = re.compile("maxconns ([0-9]+)")
 
 		last_node = None
 		for node in lists:
 			try:
-				if options.service and last_node != node[0]:
-					print('===============================================================')
-					print ('[%s] system memory' % node[0]);
-					do_ssh_command(node[0], 'free') # run every server
-					last_node = node[0]
-					print('---------------------------------------------------------------')
+				if options.service and last_node != node.ip:
+					print('===================================================================================')
+					print ('[%s] system memory' % node.ip);
+					do_ssh_command(node.ip, 'free') # run every server
+					last_node = node.ip
+					print('-----------------------------------------------------------------------------------')
 
-				result = do_arcus_command(node[0], node[1], 'stats\n')
-
+				result = node.do_arcus_command('stats')
 				m_limit = re_limit.search(result)
 				m_bytes = re_bytes.search(result)
+				m_curr_conn = re_curr_conn.search(result)
 
-				if m_limit == None or m_bytes == None:
-					print ('[%s:%s(%s)]\t\tstats failed!!' % (node[0], node[1], node[2]))
+				result = node.do_arcus_command('config maxconns')
+				m_maxconns = re_maxconns.search(result)
+
+				if m_limit == None or m_bytes == None or m_maxconns == None or m_curr_conn == None:
+					print ('%s\t\tstats failed!!' % (node))
 					continue
 				
 				limit = int(m_limit.groups()[0]) / 1024 /  1024
 				used = int(m_bytes.groups()[0]) / 1024 / 1024
+				curr_conn = int(m_curr_conn.groups()[0])
+				maxconns = int(m_maxconns.groups()[0])
 
-				print ('[%s:%s(%s)]\t\t(%d/%d) %f%%)' % (node[0], node[1], node[2], used, limit, used/limit*100))
+				print ('%s\t\tMEM: (%d/%d) %f%%, CONN: (%d/%d)' % (node, used, limit, used/limit*100, curr_conn, maxconns))
 
 			except Exception as e:
-				print ('[%s:%s(%s)]\t\tFAILED!!' % (node[0], node[1], node[2]))
+				print ('%s\t\tFAILED!!' % (node))
 				print(e)
-		print('===============================================================')
+		print('===================================================================================')
 
 
