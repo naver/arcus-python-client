6d9b4253690fd4972f1d39a25b1f08ae34423151
Lee, Ki-Yeul <lynix94@gmail.com>
2014-10-24 11:48:13 +0900

    add copy & read_tree option for zk_util.py

--- zk_util.py
+++ zk_util.py
@@ -22,10 +22,8 @@ import re
 from optparse import OptionParser
 
 
-def do_zookeeper_read(addr, path):
+def do_zookeeper_read(zk, path):
 	print(path)
-	zk = KazooClient(addr)
-	zk.start()
 	data, stat = zk.get(path)
 	print('node info:', data)
 	print('node stat:', stat)
@@ -33,32 +31,53 @@ def do_zookeeper_read(addr, path):
 	children = zk.get_children(path)
 	print('node children:', children)
 
-def do_zookeeper_create(addr, path, value):
+	return (data, stat, children)
+
+
+def do_zookeeper_read_tree(zk, path):
 	print(path)
-	zk = KazooClient(addr)
-	zk.start()
+	data, stat = zk.get(path)
+	print('node info:', data)
+	print('node stat:', stat)
+
+	children = zk.get_children(path)
+	print('node children:', children)
+
+	for child in children:
+		do_zookeeper_read_tree(zk, path + '/' + child)
+
+
+def do_zookeeper_create(zk, path, value):
+	print(path)
 	zk.create(path, bytes(value, 'utf-8'))
 
-	do_zookeeper_read(addr, path)
+	do_zookeeper_read(zk, path)
 
-def do_zookeeper_delete(addr, path):
+def do_zookeeper_delete(zk, path):
 	print(path)
-	zk = KazooClient(addr)
-	zk.start()
 	zk.delete(path)
 
 	try:
-		do_zookeeper_read(addr, path)
+		do_zookeeper_read(zk, path)
 	except kazoo.exceptions.NoNodeError:
 		print('deleted')
 	
-def do_zookeeper_update(addr, path, value):
+def do_zookeeper_update(zk, path, value):
 	print(path)
-	zk = KazooClient(addr)
-	zk.start()
 	zk.set(path, bytes(value, 'utf-8'))
 
-	do_zookeeper_read(addr, path)
+	do_zookeeper_read(zk, path)
+	
+
+def do_zookeeper_copy(zk_src, src, zk_dst, dst):
+	data, stat, children = do_zookeeper_read(zk_src, path)
+	print ('## copy %s -> %s (%s)' % (src, dst + src, data))
+	zk_dst.create(dst + src, data)
+
+	for child in children:
+		zookeeper_copy(zk_src, src + '/' + child, zk_dst, dst)
+	
+	
 
 
 if __name__ == '__main__':
@@ -72,18 +91,29 @@ if __name__ == '__main__':
 	parser.add_option('-c', '--create', dest='create', default='', help='zookeeper node create')
 	parser.add_option('-d', '--delete', dest='delete', default=False, help='zookeeper node delete', action='store_true')
 	parser.add_option('-u', '--update', dest='update', default='', help='zookeeper node update')
+	parser.add_option('', '--copy', dest='copy', default='', help='zookeeper copy addr:port/new_path')
+	parser.add_option('', '--read_tree', dest='read_tree', default=False, help='zookeeper node read tree', action='store_true')
 
 	(options, args) = parser.parse_args()
 
+	zk = KazooClient(options.address)
+	zk.start()
+
 	if options.read:
-		do_zookeeper_read(options.address, options.node)
+		do_zookeeper_read(zk, options.node)
 	elif options.create != '':
-		do_zookeeper_create(options.address, options.node, options.create)
+		do_zookeeper_create(zk, options.node, options.create)
 	elif options.delete:
-		do_zookeeper_delete(options.address, options.node)
+		do_zookeeper_delete(zk, options.node)
 	elif options.update != '':
-		do_zookeeper_update(options.address, options.node, options.update)
-
-
-	parser.print_usage();
+		do_zookeeper_update(zk, options.node, options.update)
+	elif options.copy != '':
+		dest_addr, dest_path = options.copy.split('/', 1)
+		zk_dest = KazooClient(dest_addr)
+		zk_dest.start()
+		do_zookeeper_copy(zk, options.node, zk_dest, '/' + dest_path)
+	elif options.read_tree:
+		do_zookeeper_read_tree(zk, options.node)
+	else:
+		parser.print_usage();
 
